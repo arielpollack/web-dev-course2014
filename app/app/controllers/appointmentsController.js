ce9541d5e2a8d17242266fbd4c8af5c8eb24e9b7
Ariel Pollack <pollack.ariel@gmail.com>
2015-01-11 00:20:59 +0200

    - more css and functionality
    - directive to more convenient development

--- app/app/controllers/appointmentsController.js
+++ app/app/controllers/appointmentsController.js
@@ -11,4 +11,31 @@ app.controller('AppointmentsController', ['$scope', '$http', function($scope, $h
 			console.error(data);
 		});
 
+}])
+.directive('userAppointment', [function(){
+	
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		scope: {
+			apt: '=info'
+		}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		templateUrl: 'app/directives/appointment.html',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			$scope.delete = function(id) {
+				var realy = confirm("Are you sure?");
+				if (realy === true) {
+					console.log("Will delete " + id);
+				}
+			}
+		}
+	};
 }]);
